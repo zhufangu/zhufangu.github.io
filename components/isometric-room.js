7adea8aa8b99d7e27bc6d42cf6ec40bf064ebdd7
zhufangu <75535393+zhufangu@users.noreply.github.com>
2024-11-03 01:33:07 -0500

    Remove unused imports

--- components/isometric-room.js
+++ components/isometric-room.js
@@ -1,4 +1,5 @@
-import { useState, useEffect, useRef, useCallback } from 'react'
+// import { useState, useEffect, useRef, useCallback } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { Box, Spinner } from '@chakra-ui/react'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
