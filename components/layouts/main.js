ae3b677546878add53465ae513b1d229c22a9e6f
zhufangu <75535393+zhufangu@users.noreply.github.com>
2025-10-16 00:25:43 -0500

    remove 3D model object

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,27 +1,22 @@
 import Head from 'next/head'
 import Navbar from '../navbar.js'
 import { Box, Container } from '@chakra-ui/react'
-import IsometricRoom from '../isometric-room'
-import NoSsr from '../no-ssr'
 
 const Main = ({ children, router }) => {
-    return (
-        <Box as ="main" pb={8}>
-            <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <title>Zhufan Gu - Homepage</title>  
-            </Head>
+  return (
+    <Box as="main" pb={8}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Zhufan Gu - Homepage</title>
+      </Head>
 
-            <Navbar path={router.asPath} />
-            
-            <Container maxW="container.md" pt={14}>
-                <NoSsr>
-                    <IsometricRoom />
-                </NoSsr>
-                {children}
-            </Container>        
-        </Box>
-    )
+      <Navbar path={router.asPath} />
+
+      <Container maxW="container.md" pt={14}>
+        {children}
+      </Container>
+    </Box>
+  )
 }
 
 export default Main
