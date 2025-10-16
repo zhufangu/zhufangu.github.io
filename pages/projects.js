ad66de9b4ac3efcd26a5366b8ced7cb42747f292
zhufangu <75535393+zhufangu@users.noreply.github.com>
2025-10-16 03:05:40 -0500

    Add Pantry AI project

--- pages/projects.js
+++ pages/projects.js
@@ -1,55 +1,76 @@
-import { Container, Heading, SimpleGrid, Divider } from "@chakra-ui/react";
-import Section from "../components/section";
-import { ProjectGridItem } from "../components/grid-item";
-import Layout from "../components/layouts/article";
+import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import Section from '../components/section'
+import { ProjectGridItem } from '../components/grid-item'
+import Layout from '../components/layouts/article'
 
-import thumbTindog from "../public/images/projects/tindog/tindog_banner.jpg"
-import thumbAsc from "../public/images/projects/asc/asc_banner.jpeg"
-import thumbDrumKit from "../public/images/projects/drumkit/drumkit_banner.jpg"
-import thumbOed from "../public/images/projects/oed/oed_banner.jpeg"
+import thumbTindog from '../public/images/projects/tindog/tindog_banner.jpg'
+import thumbAsc from '../public/images/projects/asc/asc_banner.jpeg'
+import thumbDrumKit from '../public/images/projects/drumkit/drumkit_banner.jpg'
+import thumbOed from '../public/images/projects/oed/oed_banner.jpeg'
+import thumbPantryAI from '../public/images/projects/pantryai/pantryai_banner.jpg'
 
 const Projects = () => {
-    return (
-        <Layout title="Projects">
-            <Container>
-                <Heading as="h3" fontSize={20} mb={4}>
-                    Software Development
-                </Heading>
+  return (
+    <Layout title="Projects">
+      <Container>
+        <Heading as="h3" fontSize={20} mb={4}>
+          Software Development
+        </Heading>
 
-                <SimpleGrid columns={[1, 1, 2]} gap={6}>
-                    <Section>
-                        <ProjectGridItem id="tindog" title="Tindog" thumbnail={thumbTindog}>
-                            Find the love of your dog&apos;s life or your money back.
-                        </ProjectGridItem>
-                    </Section>
-                    <Section>
-                        <ProjectGridItem id="asc" title="Algebraic Simplification Cesar" thumbnail={thumbAsc}>
-                            Unit testing and build bridges between Rust and Python.
-                        </ProjectGridItem>
-                    </Section>
-                    <Section>
-                        <ProjectGridItem id="oed" title="Open Energy Dashboard" thumbnail={thumbOed}>
-                        Ensuring Precision in Energy Data: A Journey Through OED Testing
-                        </ProjectGridItem>
-                    </Section>
-                    <Section>
-                        <ProjectGridItem id="drumkit" title="Drum Kit" thumbnail={thumbDrumKit}>
-                            A simple online drum kit.
-                        </ProjectGridItem>
-                    </Section>
-                </SimpleGrid>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section>
+            <ProjectGridItem
+              id="pantryai"
+              title="Pantry AI"
+              thumbnail={thumbPantryAI}
+            >
+              Leverage AI to generate and customize recipes.
+            </ProjectGridItem>
+          </Section>
+          <Section>
+            <ProjectGridItem id="tindog" title="Tindog" thumbnail={thumbTindog}>
+              Find the love of your dog&apos;s life or your money back.
+            </ProjectGridItem>
+          </Section>
+          <Section>
+            <ProjectGridItem
+              id="asc"
+              title="Algebraic Simplification Cesar"
+              thumbnail={thumbAsc}
+            >
+              Unit testing and build bridges between Rust and Python.
+            </ProjectGridItem>
+          </Section>
+          <Section>
+            <ProjectGridItem
+              id="oed"
+              title="Open Energy Dashboard"
+              thumbnail={thumbOed}
+            >
+              Ensuring Precision in Energy Data: A Journey Through OED Testing
+            </ProjectGridItem>
+          </Section>
+          <Section>
+            <ProjectGridItem
+              id="drumkit"
+              title="Drum Kit"
+              thumbnail={thumbDrumKit}
+            >
+              A simple online drum kit.
+            </ProjectGridItem>
+          </Section>
+        </SimpleGrid>
 
-                <Section delay={0.2}>
-                    <Divider my={6} />
+        <Section delay={0.2}>
+          <Divider my={6} />
 
-                    <Heading as="h3" fontSize={20} mb={4}>
-                        User Experience
-                    </Heading>
-                </Section>
-
-            </Container>
-        </Layout>
-    )
+          <Heading as="h3" fontSize={20} mb={4}>
+            User Experience
+          </Heading>
+        </Section>
+      </Container>
+    </Layout>
+  )
 }
 
 export default Projects
---
+++ pages/projects/pantryai.js
@@ -0,0 +1,105 @@
+import {
+  Container,
+  Badge,
+  Link,
+  List,
+  ListItem,
+  UnorderedList
+} from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Title, ProjectImage, Meta } from '../../components/projects'
+import P from '../../components/paragraph'
+import Layout from '../../components/layouts/article'
+
+const Project = () => {
+  return (
+    <Layout title="Pantry AI">
+      <Container>
+        <Title>
+          Pantry AI <Badge>2025</Badge>
+        </Title>
+        <P>
+          A multimodal full-stack web application that leverages AI to create
+          and customize recipes. It allows users to generate personalized
+          recipes by either describing their needs in natural language or by
+          uploading a picture of their ingredients. The project features a
+          sleek, user-friendly interface with modern and classic layouts to
+          provide a seamless user experience across different devices.
+        </P>
+        <List ml={4} my={4}>
+          <ListItem>
+            <Meta>Website</Meta>
+            <Link href="https://kaifan.vercel.app/">
+              https://kaifan.vercel.app/ <ExternalLinkIcon mx="2px" />
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Meta>Features</Meta>
+            <UnorderedList my={4}>
+              <ListItem>
+                Dual Input Modes: The application offers two primary ways to
+                generate a recipe: a text-based chat interface and an image
+                recognition tab.
+              </ListItem>
+              <ListItem>
+                AI-Powered Ingredient Recognition: Users can upload a photo of
+                their ingredients, and the system automatically identifies them
+                using a vision model, offering a list of identified ingredients
+                that can be edited.
+              </ListItem>
+              <ListItem>
+                Conversational Recipe Optimization: The app acts as an "AI
+                chef," allowing users to refine and optimize a generated recipe
+                by chatting with the system. It can handle requests such as
+                "simplify the steps," "reduce calories," or "add more
+                vegetables".
+              </ListItem>
+              <ListItem>
+                AI Image Generation: Once a recipe is finalized, the application
+                can generate a high-quality visual representation of the dish,
+                creating a more engaging user experience.
+              </ListItem>
+            </UnorderedList>
+          </ListItem>
+          <ListItem>
+            <Meta>Technology</Meta>
+            <UnorderedList my={4}>
+              <ListItem>
+                <strong>Frontend</strong>: Next.js, React, Tailwind CSS,
+                TypeScript.
+              </ListItem>
+              <ListItem>
+                <strong>Backend</strong>: FastAPI, Rest API.
+              </ListItem>
+              <ListItem>
+                <strong>Models</strong>: GPT-4o-mini, Qwen-image.
+              </ListItem>
+            </UnorderedList>
+          </ListItem>
+        </List>
+        <ProjectImage
+          src="/images/projects/pantryai/pantryai_banner.jpg"
+          alt="Pantry AI"
+        />
+        <ProjectImage
+          src="/images/projects/pantryai/pantryai_01.jpg"
+          alt="Pantry AI"
+        />
+        <ProjectImage
+          src="/images/projects/pantryai/pantryai_02.jpg"
+          alt="Pantry AI"
+        />
+        <ProjectImage
+          src="/images/projects/pantryai/pantryai_03.jpg"
+          alt="Pantry AI"
+        />
+        <ProjectImage
+          src="/images/projects/pantryai/pantryai_04.jpg"
+          alt="Pantry AI"
+        />
+      </Container>
+    </Layout>
+  )
+}
+
+export default Project
