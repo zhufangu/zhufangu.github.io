c752f93023c9a270a238e3c56ecc3e979392fc50
zhufangu <75535393+zhufangu@users.noreply.github.com>
2026-06-12 13:42:09 -0500

    Remove @vercel/analytics dependency and clean up related code; update AnimatePresence usage in components

--- components/grid-item.js
+++ components/grid-item.js
@@ -4,24 +4,6 @@ import { Box, Text, LinkBox, LinkOverlay } from '@chakra-ui/react'
 import { Global } from '@emotion/react'
 // import { Children } from "react";
 
-export const GridItem = ({ children, href, title, thumbnail }) => (
-  <Box w="100%" align="center">
-    <LinkBox cursor="pointer">
-      <Image
-        src={thumbnail}
-        alt={title}
-        className="grid-item-thumbnail"
-        placeholder="blur"
-        loading="lazy"
-      />
-      <LinkOverlay href={href} target="_blank">
-        <Text mt={2}>{title}</Text>
-      </LinkOverlay>
-      <Text fontSize={14}>{children}</Text>
-    </LinkBox>
-  </Box>
-)
-
 export const ProjectGridItem = ({ children, id, title, thumbnail }) => (
   <Box w="100%" align="center">
     <LinkBox cursor="pointer">
